c33c95f5c3d392ad02e5a9e486cea605298f097b
almanjenga <143386564+almanjenga@users.noreply.github.com>
2024-07-17 10:12:11 +0300

    database

---
+++ database/migrations/2024_07_16_160306_add_appointment_time_to_appointments.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('appointments', function (Blueprint $table) {
+            $table->dateTime('appointment_datetime'); // Change to dateTime type to store date and time
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('appointments', function (Blueprint $table) {
+            $table->dropColumn('appointment_datetime');
+        });
+    }
+};
